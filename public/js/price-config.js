b07ed95b5874743d165aa37e638dcc735b6aa962
Cysteine12 <gbengapeace2001@gmail.com>
2023-05-20 17:14:34 +0100

    Update price tags

--- public/js/price-config.js
+++ public/js/price-config.js
@@ -9,6 +9,7 @@ const new_papers = {
     5: [ 80500, 70500, 95000, 90500 ],
     6: [ 174500, 164500, 174500, 154500 ],
 }
+
 // increment on vehicle_license with 3400
 const renew_papers = {
     vehicle_license: 3400,
@@ -23,35 +24,48 @@ const renew_papers = {
     truck_trailer_permit: 6000,
     mid_year_permit: 4700
 }
+
 // Order of array is: private | commercial 
 const change_of_ownership = {
     change_of_ownership: [ 86000, 95900 ],
-    change_of_ownership_existing: [ 7100, 80900 ]
+    change_of_ownership_existing: [ 7100, 80900 ],
+    personalized_custom_number: 305000
 }
+
+// Order of object is: uber_taxify_ridehailng | private | commercial
 // vehicle price x ratio(below) = final amount
 const comprehensive_insurance = {
-    uber_taxify_ridehailng: 0.03,
-    private: 0.025,
-    commercial: 0.035
+    1: 0.03,
+    2: 0.025,
+    3: 0.035
 }
+
+// Order of object is: national_3_years_validity | national_5_years_validity | international_1_year
 const driver_license = {
-    national_3_years_validity: 21000,
-    national_5_years_validity: 26000,
-    international_1_year: 18000
+    1: 21000,
+    2: 26000,
+    3: 18000
 }
-const other_vehicle_permits = {
-    riders_card: 7500,
-    federal_mid_year_truck_permit: 3500,
-    reprint_police_cmr: 4000,
-    light_duty_permit: 5000,
-    change_of_vehicle_engine: 10500,
-    heavy_and_light_duty_permit: 4000,
-    auto_reg_database: 9500,
-    reprint_regular_number_plate: 33000,
-    reprint_customized_number_plate: 80000
+
+// Order of object is: riders_card:1 | federal_mid_year_truck_permit:2 | reprint_police_cmr:3
+// light_duty_permit:4 | change_of_vehicle_engine:5 | heavy_and_light_duty_permit:6
+// auto_reg_database:7 | reprint_regular_number_plate:8 | reprint_customized_number_plate:9
+const other_permits = {
+    1: 7500,
+    2: 3500,
+    3: 4000,
+    4: 5000,
+    5: 10500,
+    6: 4000,
+    7: 9500,
+    8: 33000,
+    9: 80000
 }
 let total_price = 0
+let x1 = 0
+let x2 = 0
 
+// ==================================================================================//
 
 const mountTotal = (total) => {
     let x = document.querySelector('input[type=submit]')
@@ -62,8 +76,7 @@ const mountTotal = (total) => {
 
 
 // NEW VEHICLE PAPERS
-let x1 = 0
-let x2 = 0
+
 const newPapers = (v1, v2) =>  {
     if (v1) x1 = v1
     if (v2) x2 = v2
@@ -112,5 +125,30 @@ const renewPapers = (e, x, y) => {
     // TOTAL PRICE
     total_price = collated_price + license_price
 
+    mountTotal(total_price)
+}
+
+// DRIVER LICENSE
+
+const driverLicense = (x) =>  {
+    total_price = driver_license[x]
+    mountTotal(total_price)
+}
+
+// COMPREHENSIVE INSURANCE
+
+const comprehensiveInsurance = (x) => {
+    if (x) x1 = x
+    if (x1) {
+        let ratio = document.getElementById('vehicle_price').value
+        total_price = Math.ceil(comprehensive_insurance[x1] * ratio)
+        mountTotal(total_price)
+    }
+}
+
+// OTHER PERMITS
+
+const otherPermits = (x) => {
+    total_price = other_permits[x]
     mountTotal(total_price)
 }
